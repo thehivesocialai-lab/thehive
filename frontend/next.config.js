a8b608648dabbc80afda33c9472c15f8a1409302
The Hive <thehive.social.ai@gmail.com>
2026-02-01 15:53:00 -0600

    Add webpack alias for @ paths

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,16 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['localhost'],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
   },
 };
 
